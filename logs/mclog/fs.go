515d7287b3bac1d22a4fe4cd17833db2cec99bef
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2025-03-03 22:54:27 -0300

    Update mclog server
    Signed-off-by: Matheus Sampaio Queiroga <srherobrine20@gmail.com>

--- logs/mclog/fs.go
+++ logs/mclog/fs.go
@@ -6,7 +6,9 @@ import (
 	"io"
 	"io/fs"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 
 	"sirherobrine23.com.br/go-bds/go-bds/overlayfs"
 )
@@ -25,9 +27,27 @@ type File interface {
 type FileSystem interface {
 	fs.FS
 	Create(string) (File, error)
+	Mkdir(string, fs.FileMode) error
+}
+
+func MkdirAll(fss FileSystem, dir string, perm fs.FileMode) error {
+	paths := strings.Split(filepath.ToSlash(dir), "/")
+	for pathIndex := range paths {
+		if err := fss.Mkdir(path.Join(paths[:pathIndex]...), perm); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func CreateID(fss FileSystem, dir string) (string, File, error) {
+	dir = path.Clean(filepath.ToSlash(dir))
+	if _, err := fs.Stat(fss, dir); err != nil {
+		if err := MkdirAll(fss, dir, 0755); err != nil {
+			return "", nil, err
+		}
+	}
+
 	id := make([]byte, 16)
 	for attemps := 8; attemps > 0; attemps-- {
 		if _, err := rand.Read(id); err != nil {
@@ -36,7 +56,9 @@ func CreateID(fss FileSystem, dir string) (string, File, error) {
 		id[6] = (id[6] & 0x0f) | 0x40
 		id[8] = (id[8] & 0x3f) | 0x80
 		name := fmt.Sprintf("%x", id)
-		if file, err := fss.Create(filepath.Join(dir, name)); err == nil {
+		if _, err := fs.Stat(fss, path.Join(dir, name)); err == nil {
+			continue // Skip exists
+		} else if file, err := fss.Create(path.Join(dir, name)); err == nil {
 			return name, file, nil
 		}
 	}
@@ -47,6 +69,14 @@ func CreateID(fss FileSystem, dir string) (string, File, error) {
 type Local string
 
 func (local Local) Open(name string) (fs.File, error) { return os.OpenInRoot(string(local), name) }
+func (local Local) Mkdir(name string, perm fs.FileMode) error {
+	r, err := os.OpenRoot(string(local))
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+	return r.Mkdir(name, perm.Perm())
+}
 func (local Local) Create(name string) (File, error) {
 	r, err := os.OpenRoot(string(local))
 	if err != nil {
@@ -59,6 +89,9 @@ func (local Local) Create(name string) (File, error) {
 type Mergefs overlayfs.FsMergeFs
 
 func (fss Mergefs) Open(name string) (fs.File, error) { return overlayfs.FsMergeFs(fss).Open(name) }
+func (fss Mergefs) Mkdir(name string, perm fs.FileMode) error {
+	return overlayfs.FsMergeFs(fss).MergedFS.Mkdir(filepath.Join(fss.Subdir, filepath.Clean(name)), perm.Perm())
+}
 func (fss Mergefs) Create(name string) (File, error) {
 	if fss.MergedFS == nil {
 		return nil, fs.ErrInvalid
--- logs/mclog/server.go
+++ logs/mclog/server.go
@@ -1,12 +1,17 @@
 package mclog
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"io/fs"
+	"maps"
 	"net/http"
+	"path"
+	"runtime"
 	"slices"
 	"strings"
 
@@ -22,6 +27,7 @@ func writeJson(w http.ResponseWriter, code int, v any) {
 }
 
 func NewHandler(limits Limits, fss FileSystem) http.Handler {
+	fss.Mkdir("v2", 0755) // Ignore error's
 	mux, v1, v2 := http.NewServeMux(), http.NewServeMux(), http.NewServeMux()
 
 	// mclogs limits
@@ -184,7 +190,176 @@ func NewHandler(limits Limits, fss FileSystem) http.Handler {
 		writeJson(w, 200, ConvertLogs("", log))
 	})
 
+	// Server v2
+	v2Info, _ := json.MarshalIndent(map[string]any{"runtime": runtime.Version(), "limits": limits}, "", "  ")
+	v2.HandleFunc("GET /{$}", func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(v2Info)
+		w.Write(nil)
+	})
+
+	v2.HandleFunc("POST /{$}", func(w http.ResponseWriter, r *http.Request) {
+		logString, parsedLogs := []string{}, map[string]logs.Log{}
+		if slices.Contains(r.Header.Values("Content-Type"), "application/octet-stream") {
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				writeJson(w, 400, map[string]any{"error": fmt.Errorf("cannot read json body: %s", err).Error()})
+				return
+			}
+			logString = append(logString, string(body))
+		} else if slices.Contains(r.Header.Values("Content-Type"), "multipart/form-data") {
+			if err := r.ParseMultipartForm(10 << 20); err != nil {
+				writeJson(w, 400, map[string]any{"error": fmt.Errorf("cannot parse form: %s", err).Error()})
+				return
+			}
+			for _, content := range r.MultipartForm.Value {
+				logString = append(logString, content...)
+			}
+			for _, content := range r.MultipartForm.File {
+				for _, file := range content {
+					info, err := file.Open()
+					if err != nil {
+						writeJson(w, 400, map[string]any{"error": fmt.Errorf("cannot read json body: %s", err).Error()})
+						return
+					}
+					defer info.Close()
+					body, err := io.ReadAll(info)
+					if err != nil {
+						writeJson(w, 400, map[string]any{"error": fmt.Errorf("cannot read json body: %s", err).Error()})
+						return
+					}
+					logString = append(logString, string(body))
+				}
+			}
+		} else if slices.Contains(r.Header.Values("Content-Type"), "application/x-www-form-urlencoded") {
+			if err := r.ParseForm(); err != nil {
+				writeJson(w, 400, map[string]any{"error": fmt.Errorf("cannot parse form: %s", err).Error()})
+				return
+			}
+			for _, content := range r.Form {
+				logString = append(logString, content...)
+			}
+		} else if slices.Contains(r.Header.Values("Content-Type"), "application/json") {
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				writeJson(w, 400, map[string]any{"error": fmt.Errorf("cannot read json body: %s", err).Error()})
+				return
+			}
+
+			var data any
+			if err = json.Unmarshal(body, &data); err != nil {
+				writeJson(w, 400, map[string]any{"error": fmt.Errorf("cannot parse json: %s", err).Error()})
+				return
+			}
+
+			switch v := data.(type) {
+			case []string:
+				logString = v
+			case map[string]string:
+				logString = slices.Collect(maps.Values(v))
+			default:
+				writeJson(w, 400, map[string]any{"error": "invalid data type", "tips": "[]string, map[string]string"})
+				return
+			}
+		} else {
+			writeJson(w, 400, map[string]any{"error": "require Content-Type in header"})
+			return
+		}
+
+		err := error(nil)
+		for _, log := range logString {
+			log = strings.TrimSpace(log)
+			sum := sha256.Sum256([]byte(log))
+			if parsedLogs[hex.EncodeToString(sum[:])], err = logs.ParseString(log); err != nil {
+				writeJson(w, 400, map[string]any{"error": fmt.Errorf("cannot parse json: %s", err).Error()})
+				return
+			}
+		}
+
+		writeJson(w, 200, parsedLogs)
+	})
+
+	v2.HandleFunc("POST /raw", func(w http.ResponseWriter, r *http.Request) {
+		if !slices.Contains(r.Header.Values("Content-Type"), "application/octet-stream") {
+			writeJson(w, 400, map[string]any{"error": "require application/octet-stream on Content-Type header"})
+			return
+		}
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			writeJson(w, 400, map[string]any{"error": fmt.Errorf("cannot get full body: %s", err).Error()})
+			return
+		}
+
+		body = []byte(strings.TrimSpace(string(body)))
+		if _, err = logs.ParseBuffer(body); err != nil {
+			writeJson(w, 400, map[string]any{"error": fmt.Errorf("invalid log: %s", err).Error()})
+			return
+		}
+
+		fileSum := sha256.Sum256(body)
+		fileID := hex.EncodeToString(fileSum[:])
+		remoteFile, err := fss.Open(path.Join("v2", fileID))
+		if remoteFile != nil {
+			remoteFile.Close() // Close file
+		}
+
+		if err != nil {
+			if !errors.Is(err, fs.ErrNotExist) {
+				writeJson(w, 400, map[string]any{"error": fmt.Errorf("fs error: %s", err).Error()})
+				return
+			}
+			create, err := fss.Create(path.Join("v2", fileID))
+			if err != nil {
+				writeJson(w, 400, map[string]any{"error": fmt.Errorf("fs error: %s", err).Error()})
+				return
+			}
+			create.Write(body)
+			create.Close()
+		}
+		writeJson(w, 200, map[string]any{"id": fileID})
+	})
+
+	v2.HandleFunc("GET /raw/{id}", func(w http.ResponseWriter, r *http.Request) {
+		file, err := fss.Open(path.Join("v2", r.PathValue("id")))
+		if err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				writeJson(w, 404, map[string]any{"error": "file not exists"})
+			} else {
+				writeJson(w, 400, map[string]any{"error": fmt.Errorf("fs error: %s", err).Error()})
+			}
+			return
+		}
+		defer file.Close()
+		io.Copy(w, file)
+	})
+	v2.HandleFunc("GET /insight/{id}", func(w http.ResponseWriter, r *http.Request) {
+		file, err := fss.Open(path.Join("v2", r.PathValue("id")))
+		if err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				writeJson(w, 404, map[string]any{"error": "file not exists"})
+			} else {
+				writeJson(w, 400, map[string]any{"error": fmt.Errorf("fs error: %s", err).Error()})
+			}
+			return
+		}
+		defer file.Close()
+		body, err := io.ReadAll(file)
+		if err != nil {
+			writeJson(w, 400, map[string]any{"error": fmt.Errorf("read: %s", err).Error()})
+			return
+		}
+		log, err := logs.ParseBuffer(body)
+		if err != nil {
+			writeJson(w, 400, map[string]any{"error": fmt.Errorf("logs parse: %s", err).Error()})
+			return
+		}
+		writeJson(w, 200, log)
+	})
+
 	mux.Handle("/1/", http.StripPrefix("/1", v1))
+	mux.Handle("/v1/", http.StripPrefix("/v1", v1))
 	mux.Handle("/v2/", http.StripPrefix("/v2", v2))
 	return mux
 }
